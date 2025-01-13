55cc7d422ffb97951bc6aae74f2df0ae0f2928ff
okuno <g2050230@cc.kyoto-su.ac.jp>
2025-01-13 18:33:29 +0900

    update K in each step.

--- calculation/numerical/simulation_def.py
+++ calculation/numerical/simulation_def.py
@@ -70,12 +70,12 @@ class Simulation(WaveResolutions):
 
         self.mat_num = self.num+1
         #self.A need to be changed!!
-        self.A = self.K * self.dt / self.dn**2
+        #self.A = self.K * self.dt / self.dn**2
         self.B = self.dt * self.N**2 * np.sin(self.alpha) / self.gamma
         self.C = -self.dt * self.gamma * np.sin(self.alpha)
 
         self.w= self.make_w_init()
-        self.matrix = self.make_block_matrix() #change!
+       # self.matrix = self.make_block_matrix() #change!
         #time--------------
         self.day = 4
         self.day2sec = 24 * 3600
@@ -87,45 +87,68 @@ class Simulation(WaveResolutions):
     def make_w_init(self):
         return np.zeros((self.mat_num)*2).reshape((self.mat_num)*2,1)
 
-    #matrix
+    #block matrix
+    def updateK(self, K): #K: column vector whose length is equal to self.mat_num
+        coef = self.dt / self.dn**2
+        E = np.ones_like(K)
+        A = coef * K
+        AA = E - 2*coef*K
+        return A, AA
+        
     def make_off_diagonal(self, val):
         a = np.diag([val]*self.mat_num)
         a[0,0], a[self.mat_num-1, self.mat_num-1] = 0, 0
         return a
 
-    def make_1_1(self):
+    def make_1_1(self, A, AA):
         a = np.zeros((self.mat_num, self.mat_num))
         for i in range(1, self.num-1):
-            a[i,i+1] = self.A
-            a[i+1,i] = self.A
-        np.fill_diagonal(a, 1-2*self.A)
+            a[i,i+1] = A[i]
+            a[i+1,i] = A[i]
+            a[i,i] = AA[i]
+        #np.fill_diagonal(a, AA)#
         a[0,0], a[self.mat_num-1,self.mat_num-1] = 0, 0
         return a
 
-    def make_2_2(self):
-        a = self.make_1_1()
-        a[1,0] = self.A
+    def make_2_2(self, A, AA):
+        a = self.make_1_1(A, AA)
+        a[1,0] = A[1]
         return a
 
-    def make_block_matrix(self):
-        a = self.make_1_1()
+    def update_block_matrix(self, KK):
+        A, AA = self.updateK(KK)
+        a = self.make_1_1(A, AA)
         b = self.make_off_diagonal(self.B)
         c = self.make_off_diagonal(self.C)
-        d = self.make_2_2()
+        d = self.make_2_2(A, AA)
         
         block_matrix = np.block([
             [a,b],
             [c,d]
             ])
+        """
+        print("a:", a)
+        print("b:", b)
+        print("c:", c)
+        print("d:", d)
+        print(A, AA)
+        """
         return block_matrix
     
     def run_simulation(self, output_path, dt):
         surf_con = xr.open_dataset("TestGroundTheta0.nc")
         surface_theta = surf_con.theta_0.values
+        K_ds = xr.open_dataset("K/constant_100.nc")
+        K_vec = K_ds.K.values.reshape(K_ds.K.values.shape[1], K_ds.K.values.shape[0])
         
         for m, t in enumerate(self.times):
             num = m%(24*3600*10)
-            self.w = self.matrix @ self.w
+            K_ = K_vec[num]
+            #print(K_, K_.shape)
+            #print(self.dn, self.l_plus, self.N_alpha)
+            matrix = self.update_block_matrix(K_)
+            #input("stop")
+            self.w = matrix @ self.w
             #self.w[self.num+1] = self.Theta*np.sin(self.omega * t)
             #self.w[self.num+1] = surface_theta_0(self.Theta, self.omega, t)
             self.w[self.num+1] = surface_theta[num]
